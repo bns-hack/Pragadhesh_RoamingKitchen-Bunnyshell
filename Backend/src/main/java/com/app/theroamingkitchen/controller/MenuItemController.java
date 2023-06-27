f1a640ac4c14c8f89cd1081e952bf589df51ce97
Pragadhesh <47506237+Pragadhesh@users.noreply.github.com>
2023-06-27 11:10:02 +0530

    Update MenuItemController.java

--- Backend/src/main/java/com/app/theroamingkitchen/controller/MenuItemController.java
+++ Backend/src/main/java/com/app/theroamingkitchen/controller/MenuItemController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "https://roamingkitchen-application.bunnyenv.com")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class MenuItemController {
